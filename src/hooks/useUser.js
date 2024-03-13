85ef89b3abf4340a3eeaa3c4a46339ca008b8773
Denver <denver3542@gmail.com>
2024-03-13 11:33:41 +0800

    Update useUser.js

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -4,75 +4,80 @@ import axiosInstance, { getJWTHeader } from "../../utils/axiosConfig";
 import { clearStoredUser, setStoredUser } from "../user-storage";
 
 export async function getUser(signal) {
-    let user = await AsyncStorage.getItem("upcare_user");
-    if (!user) {
-        return null;
-    }
+  let user = await AsyncStorage.getItem("upcare_user");
+  if (!user) {
+    return null;
+  }
 
-    const { data } = await axiosInstance.get("/auth", {
-        signal, // abortSignal from React Query
-        headers: getJWTHeader(JSON.parse(user)),
-    });
-    return user ?? data;
+  const { data } = await axiosInstance.get("/auth", {
+    signal, // abortSignal from React Query
+    headers: getJWTHeader(JSON.parse(user)),
+  });
+  return data.user;
 }
 
 export const useUser = () => {
-    const queryClient = useQueryClient();
-    const { data: user, isLoading, isFetching, isFetched, refetch } = useQuery({
-        queryKey: ['user'],
-        queryFn: ({ signal }) => getUser(signal),
-        onSuccess: (received) => {
-            if (!received) {
-                clearStoredUser();
-            } else {
-                setStoredUser(received);
-            }
-        },
-        onError: (err) => {
-            console.log(err);
-            clearUser();
-        },
-    });
-
-
-    // meant to be called from useAuth
-    function updateUser(newUser) {
-        // update the user
-        queryClient.setQueryData(["user"], newUser);
-    }
-
-    // meant to be called from useAuth
-    function clearUser() {
-        // reset user to null
-        queryClient.setQueryData(["user"], null);
-        queryClient.clear();
+  const queryClient = useQueryClient();
+  const {
+    data: user,
+    isLoading,
+    isFetching,
+    isFetched,
+    refetch,
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: ({ signal }) => getUser(signal),
+    onSuccess: (received) => {
+      if (!received) {
         clearStoredUser();
+      } else {
+        setStoredUser(received);
+      }
+    },
+    onError: (err) => {
+      console.log(err);
+      clearUser();
+    },
+  });
+
+  // meant to be called from useAuth
+  function updateUser(newUser) {
+    // update the user
+    queryClient.setQueryData(["user"], newUser);
+  }
+
+  // meant to be called from useAuth
+  function clearUser() {
+    // reset user to null
+    queryClient.setQueryData(["user"], null);
+    queryClient.clear();
+    clearStoredUser();
+  }
+
+  async function addPushToken(token) {
+    let user = await AsyncStorage.getItem("upcare_user");
+    if (!user) {
+      // Logout the user
+      return null;
     }
-
-    async function addPushToken(token) {
-        let user = await AsyncStorage.getItem("upcare_user");
-        if (!user) {
-            // Logout the user
-            return null;
-        }
-        const body = {
-            pushToken: token,
-        };
-
-        const { data } = await axiosInstancee.put("/auth/push-token", body, {
-            headers: getJWTHeader(JSON.parse(user)),
-        });
-        return data;
-    }
-
-    return {
-        user,
-        isLoading,
-        isFetching,
-        isFetched,
-        refetchUser: refetch,
-        updateUser,
-        clearUser,
-        addPushToken,
+    const body = {
+      pushToken: token,
     };
+
+    const { data } = await axiosInstance.put("/auth/push-token", body, {
+      headers: getJWTHeader(JSON.parse(user)),
+    });
+    return data;
+  }
+
+  return {
+    user,
+    isLoading,
+    isFetching,
+    isFetched,
+    refetchUser: refetch,
+    updateUser,
+    clearUser,
+    addPushToken,
+  };
 };
